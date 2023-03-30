e62d30e0cf97f5fe97ac94f8b11307d7d466236b
MuhmdAfnan <muhd.afnann@gmail.com>
2023-03-30 15:55:21 +0800

    random

---
+++ quiz1.js
@@ -0,0 +1,44 @@
+//let message = "benr"
+//console.log(message) 
+//console. log(typeof message)
+
+     //message = 123
+     //console.log(message, typeof message)
+
+//let str = "BENR"
+//let num = 3523
+
+//let course = str + num
+//console.log(course, typeof course)
+
+//let str = "2" 
+//let num = 1
+
+//let total = parseInt(str) + num // parseFloat for non integer
+
+//console.log(total, typeof total)
+
+
+//let data = ["dog", 2023 , true]
+//console.log(data[0])
+//console.log(data[1])
+//console.log(data[2])
+
+//let user = {
+    //name: "Afnan",
+    //age: 24,
+    //faculty : "FKEKK"
+//}
+
+//console.log(user.name)
+//console.log(user.age)
+
+let messages = [
+    {
+        name: "usr1",
+        date : "30/3/2023",
+        msg : "shit",
+        like : 69
+    }
+]
+console.log( messages[0].msg )
